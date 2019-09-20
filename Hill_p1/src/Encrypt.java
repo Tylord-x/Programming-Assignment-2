f71680d790d889c605d2afaef2e8ff0c95fb1c04
Tyler <tylerhill420@gmail.com>
2019-09-20 17:41:50 -0400

    Updated encrypt

--- Hill_p1/src/Encrypt.java
+++ Hill_p1/src/Encrypt.java
@@ -7,13 +7,31 @@ public class Encrypt {
 	      int val2;
 	      int val3;
 	      int val4;
+	      int encVal1;
+	      int encVal2;
+	      int encVal3;
+	      int encVal4;
 	      
-	      System.out.print("Please enter 4 integers with spaces in between each: ");
+	      System.out.print("Please enter 4 single digit integers with spaces in between each: ");
 	      
 	      val1 = scnr.nextInt();
 	      val2 = scnr.nextInt();
 	      val3 = scnr.nextInt();
 	      val4 = scnr.nextInt();
+	      
+	      System.out.println("Your initial value is: " + val1 + val2 + val3 + val4);
+	      
+	      val1 = (val1 + 7) % 10;
+	      val2 = (val2 + 7) % 10;
+	      val3 = (val3 + 7) % 10;
+	      val4 = (val4 + 7) % 10;
+	      
+	      encVal1 = val3;
+	      encVal2 = val4;
+	      encVal3 = val1;
+	      encVal4 = val2;
+	      
+	      System.out.println("Your encrypted value is: " + encVal1 + encVal2 + encVal3 + encVal4);
 
 	}
 
