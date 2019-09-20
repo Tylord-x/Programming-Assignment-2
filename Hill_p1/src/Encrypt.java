25bc894abd276c5b6b359261d1dbb3ff539b7e78
Tyler <tylerhill420@gmail.com>
2019-09-20 15:10:04 -0400

    Programming assignment 2

---
+++ Hill_p1/src/Encrypt.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Encrypt {
+
+	public static void main(String[] args) {
+	      Scanner scnr = new Scanner(System.in);
+	      int val1;
+	      int val2;
+	      int val3;
+	      int val4;
+	      
+	      System.out.print("Please enter 4 integers with spaces in between each: ");
+
+	}
+
+}
