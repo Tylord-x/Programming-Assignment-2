201fb159d4dee48b4f5ff535fb54046ba93f36a4
Tyler <tylerhill420@gmail.com>
2019-09-20 19:04:37 -0400

    finished encrypt and decrypt

--- Hill_p1/src/Decrypt.java
+++ Hill_p1/src/Decrypt.java
@@ -4,6 +4,36 @@ public class Decrypt {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner (System.in);
+		int val1;
+	    int val2;
+	    int val3;
+	    int val4;
+	    int decVal1;
+	    int decVal2;
+	    int decVal3;
+	    int decVal4;
+	      
+	    System.out.print("Please enter 4 single digit integers with spaces in between each: ");
+	      
+	    decVal1 = scnr.nextInt();
+	    decVal2 = scnr.nextInt();
+	    decVal3 = scnr.nextInt();
+	    decVal4 = scnr.nextInt();
+	      
+	    System.out.println("Your decrypted value is: " + decVal1 + decVal2 + decVal3 + decVal4);
+	    
+	    val1 = decVal3;
+	    val2 = decVal4;
+	    val3 = decVal1;
+	    val4 = decVal2;
+	    
+	    val1 = (val1 + 3) % 10;
+	    val2 = (val2 + 3) % 10;
+	    val3 = (val3 + 3) % 10;
+	    val4 = (val4 + 3) % 10;
+	    
+	      
+	    System.out.println("Your original value is: " + val1 + val2 + val3 + val4);
 
 	}
 
